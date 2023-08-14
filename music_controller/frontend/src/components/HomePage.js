de1971aa65044392d59e0c3afb787ca39be5234c
AswanthAnu <aswanthanu777@gmail.com>
2023-08-14 11:54:01 +0530

    Created first level

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import RoomJoinPage from './RoomJoinPage'
+import CreateRoomPage from './CreateRoomPage'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Redirect,
+} from "react-router-dom"
+
+const HomePage = () => {
+  return (
+    <Router>
+        <Routes>
+            <Route  path="/" element={<p>This is the Homepage</p>}/>
+            <Route path='/join'  element={<RoomJoinPage />}/>
+            <Route path='/create' element={<CreateRoomPage />}/>
+        </Routes>
+    </Router>
+  )
+}
+
+export default HomePage
