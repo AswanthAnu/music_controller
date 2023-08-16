541cab869275c46a1fa344fb7543d1bb15ec96cb
AswanthAnu <aswanthanu777@gmail.com>
2023-08-16 14:18:41 +0530

    Redirect to room when in session

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import RoomJoinPage from './RoomJoinPage'
 import CreateRoomPage from './CreateRoomPage'
 import {
@@ -7,14 +7,54 @@ import {
     Route,
     Link,
     Redirect,
+    Navigate
 } from "react-router-dom"
 import Room from './Room'
+import { Grid, Typography, ButtonGroup, Button } from '@material-ui/core'
+
+
 
 const HomePage = () => {
+
+  const [roomCode, setRoomCode] = useState(null)
+
+  useEffect(() => {
+    async function fetchRoomCode() {
+      const response = await fetch("/api/user-in-room")
+      const data = await response.json()
+      setRoomCode(data.code)
+    }
+    fetchRoomCode()
+  }, [])
+
+
+
+  const renderHomePage = () => {
+    return(
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography  variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    )
+  }
   return (
     <Router>
         <Routes>
-            <Route  path="/" element={<p>This is the Homepage</p>}/>
+            <Route path="/" element={
+              roomCode ? <Navigate to={`/room/${roomCode}`} />: renderHomePage}/>
             <Route path='/join'  element={<RoomJoinPage />}/>
             <Route path='/create' element={<CreateRoomPage />}/>
             <Route path='/room/:roomCode' element={<Room />}/>
